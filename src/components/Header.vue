4a38454b5c5a4fdb59c9b475d369b540ead1b8da
Fabian <fgraeml@yahoo.de>
2023-10-27 17:45:11 +0200

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,15 @@
             <div class="container-fluid d-flex justify-content-md-center">
               <router-link to="/" class="navbar-brand text-center border p-3 p-sm-3 m-1 border-4 rounded-pill text-decoration-none">
                     <div class="d-flex gap-1 align-items-center" style="color: white;">
-                      <img class="rounded-circle" style="width: 50px;" src="https://img.freepik.com/vektoren-kostenlos/charity-logo-haende-die-herz-symbol-flache-design-vektor-illustration-unterstuetzen_53876-136265.jpg?w=826&t=st=1695111421~exp=1695112021~hmac=df7de16068fd496eb792d3934a15d4b7e524b2c65c48a1a4979cd6b1f2394a95">
+                      <img class="rounded-circle" style="width: 50px;" 
+                      src="https://img.freepik.com/vektoren-kostenlos/charity-logo-haende-die-herz-symbol-flache-design-vektor-illustration-unterstuetzen_53876-136265.jpg?w=826&t=st=1695111421~exp=1695112021~hmac=df7de16068fd496eb792d3934a15d4b7e524b2c65c48a1a4979cd6b1f2394a95">
                       <h4 class="d-none d-sm-block p-1 pt-2"> KSK Kleiderspenden für Krisenregionen e.V.</h4>
                     </div>  
               </router-link>
 
-              <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" v-if="!inGS">
+              <button class="navbar-toggler ms-auto" type="button" 
+              data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" 
+              aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" v-if="!inGS">
                 <span class="navbar-toggler-icon"></span>
               </button>
 
